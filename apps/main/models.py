5fa0f57e87ecd313388568feda0d42d2299d0bc5
unknown <alexis_ancona@hotmail.com>
2021-03-19 20:38:52 -0700

    Default tables and Person table created

--- apps/main/models.py
+++ apps/main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Person(models.Model):
+    id = models.AutoField(primary_key = True)
+    name = models.CharField(max_length = 200)
+    lastName = models.CharField(max_length = 200)
+    email = models.EmailField(max_length = 200)
