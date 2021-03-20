75e01a09278db14c62f1dfa3e6acb6fdbc2cbe93
unknown <alexis_ancona@hotmail.com>
2021-03-19 21:26:56 -0700

    Index and Create view added to Views

--- apps/main/views.py
+++ apps/main/views.py
@@ -6,3 +6,6 @@ def index(request):
     pl = Person.objects.all() 
     ctx = {'persons_list': pl}
     return render(request, 'index.html', ctx)
+
+def createPerson(request):
+    return render(request, 'create_person.html')
